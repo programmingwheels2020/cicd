98756b5041ebee698610f9810a0981f7c63835b6
Abdul Aziz <abdaziz@publicisgroupe.net>
2021-09-30 15:07:36 +0530

    welcome service

--- src/main/java/com/epsilon/springcicddemo/controllers/WelcomeController.java
+++ src/main/java/com/epsilon/springcicddemo/controllers/WelcomeController.java
@@ -3,11 +3,15 @@ package com.epsilon.springcicddemo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.epsilon.springcicddemo.services.WelcomeService;
+
 @RestController
 public class WelcomeController {
+	
+	private WelcomeService welcomeService;
 
 	@GetMapping(path = "/welcome")
 	public String welcome() {
-		return "Welcome to ooty , nice to meet you";
+		return this.welcomeService.welcome();
 	}
 }
