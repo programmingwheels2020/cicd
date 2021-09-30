d4e963b4be8da877199f5349f570f897fdb5dfb2
programmingwheels <leninlawrence111@gmail.com>
2021-09-30 14:19:27 +0530

    Initial Commit

---
+++ src/main/java/com/epsilon/springcicddemo/SpringcicddemoApplication.java
@@ -0,0 +1,13 @@
+package com.epsilon.springcicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcicddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcicddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epsilon/springcicddemo/SpringcicddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsilon.springcicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcicddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
